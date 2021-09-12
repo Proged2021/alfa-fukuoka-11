e9312d0543da57250a0209d48abf6782b39df2dd
MT472562 <88234127+MT472562@users.noreply.github.com>
2021-09-12 18:37:52 +0900

    ゲーム開始時に自動的に全画面になるように変更

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const div = document.getElementById("div");
 const yesButton = document.getElementById("yes");
 const noButton = document.getElementById("no");
 const audio = new Audio("introduction.mp3");
+var ele = document.documentElement;
 
 function hideDiv() {
 div.style.display = "none";
@@ -10,6 +11,7 @@ yesButton.addEventListener("click", () => {
 audio.play();
 video.play();
 hideDiv();
+ele.requestFullscreen();
 setTimeout(function(){
 window.location.href = 'inport.html';
 },113*1000);
@@ -17,14 +19,9 @@ window.location.href = 'inport.html';
 noButton.addEventListener("click", () => {
 video.play();
 hideDiv();
+ele.requestFullscreen();
 setTimeout(function(){
     window.location.href = 'inport.html';
     },110*1000);
 });
-
-function hoge() {
-    // 全画面表示
-    var ele = document.documentElement;
-    ele.requestFullscreen();
-  }; 
         
