6231764ba54ec57f731c40c5a59d92a585070e7f
MT472562 <88234127+MT472562@users.noreply.github.com>
2021-09-13 11:43:55 +0900

    アラートの追加

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const div = document.getElementById("div");
 const yesButton = document.getElementById("yes");
+const button1 = document.getElementById("button1");
 const noButton = document.getElementById("no");
 const audio = new Audio("introduction.mp3");
 var ele = document.documentElement;
@@ -24,4 +25,8 @@ setTimeout(function(){
     window.location.href = 'inport.html';
     },110*1000);
 });
-        
+button1.addEventListener("click", () => {
+    if (window.confirm("ストーリーをスキップしますか？")) {
+    window.location.href = 'inport.html';
+    }
+    });
